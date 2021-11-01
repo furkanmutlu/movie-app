91174568a4c6d6da25fdfd09688e3a0b449bf8b1
Furkan Mutlu <Furkan.Mutlu@tomtom.com>
2021-11-01 21:59:18 +0100

    Create SearchBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import HeroImage from "./HeroImage";
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -16,7 +17,7 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
 
     return (
         <>
@@ -27,6 +28,7 @@ const Home = () => {
                     text={state.results[0].overview}
                 />
             ) : null}
+            <SearchBar setSearchTerm={setSearchTerm} />
             <Grid header='Popular Movies'>
                 {state.results.map(movie => (
                     <Thumb 
