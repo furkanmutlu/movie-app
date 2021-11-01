cd943b238f207e66e3ef1951bd00b3ecfcea50c2
Furkan Mutlu <Furkan.Mutlu@tomtom.com>
2021-11-01 16:57:50 +0100

    Add  global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function App() {
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
