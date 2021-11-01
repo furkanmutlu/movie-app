7148beaaee9f8227c69a0c02bcb14fd941ed57e3
Furkan Mutlu <Furkan.Mutlu@tomtom.com>
2021-11-01 21:26:06 +0100

    Created HeroImage, Grid, Thumb, Spinner

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
+// Styles
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
 const Header = () => {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
+import HeroImage from "./HeroImage";
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 // Hook
-import { useHomeFetch } from '../hooks/useHomeFetch';
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 // Image
 import NoImage from "../images/no_image.jpg";
@@ -15,9 +18,32 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
     const { state, loading, error } = useHomeFetch();
 
-    console.log(state);
-
-    return <div>Home Page</div>;
+    return (
+        <>
+            {state.results[0] ? (
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+            ) : null}
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <Thumb 
+                        key={movie.id}
+                        clickable={true}
+                        image = {
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+            <Spinner />
+        </>
+    );
 };
 
 export default Home;
