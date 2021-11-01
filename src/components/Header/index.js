076288721e393f50136d979a31a52b13fbaed6d5
Furkan Mutlu <Furkan.Mutlu@tomtom.com>
2021-11-01 18:20:43 +0100

    Created Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <Content>
+                <LogoImg src={RMDBLogo} alt='RMDB Logo' />
+                <TMDBLogoImg src={TMDBLogo} alt='TMDB Logo' />
+            </Content>
+        </Wrapper>
+    );
+};
+
+export default Header;
