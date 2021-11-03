697e499ad5caa98656a5b0f28838de10bec65252
Furkan Mutlu <furkanmutluu@gmail.com>
2021-11-03 19:07:33 +0100

    MovieInfoBar component added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 // Config
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 // Components
-import BreadCrumb from './BreadCrumb';
-import Grid from './Grid';
-import Spinner from './Spinner';
-import MovieInfo from './MovieInfo';
+import BreadCrumb from "./BreadCrumb";
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 
 // Hook
-import { useMovieFetch } from '../hooks/useMovieFetch';
+import { useMovieFetch } from "../hooks/useMovieFetch";
 
 // Image
-import NoImage from '../images/no_image.jpg';
+import NoImage from "../images/no_image.jpg";
 
 const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId);
 
-    if (loading) return <Spinner />
-    if (error) return <div>Something went wrong...</div>
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
 
     return (
         <>
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
+            <MovieInfoBar
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue}
+            />
         </>
     );
 };
