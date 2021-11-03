663bff7ab8001c7629fc2e83c0ad8950bd5e1e4f
Furkan Mutlu <furkanmutluu@gmail.com>
2021-11-03 19:19:22 +0100

    Actor Component Added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,6 +10,7 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -33,6 +34,20 @@ const Movie = () => {
                 budget={movie.budget}
                 revenue={movie.revenue}
             />
+            <Grid header="Actors">
+                {movie.actors.map((actor) => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </>
     );
 };
