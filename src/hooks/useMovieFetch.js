07671630b41205225d57a2a8def475d7e51f4b01
Furkan Mutlu <furkanmutluu@gmail.com>
2021-11-03 20:31:11 +0100

    Add individual movie to sessionStorage

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
+
+// API
 import API from "../API";
+
+// Helpers
+import { isPersistedState } from "../helpers";
 
 export const useMovieFetch = (movieId) => {
     const [state, setState] = useState({});
@@ -32,8 +37,21 @@ export const useMovieFetch = (movieId) => {
             }
         };
 
+        const sessionState = isPersistedState(movieId);
+
+        if (sessionState) {
+            setState(sessionState);
+            setLoading(false);
+            return;
+        }
+
         fetchMovie();
     }, [movieId]);
+
+    // Write to sessionStorage
+    useEffect(() => {
+        sessionStorage.setItem(movieId, JSON.stringify(state));
+    }, [movieId, state]);
 
     return { state, loading, error };
 };
